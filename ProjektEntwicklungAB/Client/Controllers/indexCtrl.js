84fd243e5a044b3dbbe448cdf862c37a6027340f
cklein2 <corinna_sofie.klein@smail.fh-koeln.de>
2018-05-20 17:02:26 +0200

    changed  to  for REST

--- ProjektEntwicklungAB/Client/Controllers/indexCtrl.js
+++ ProjektEntwicklungAB/Client/Controllers/indexCtrl.js
@@ -1,51 +1,18 @@
-angular.module('controllerAsExample', [])
-  .controller('SettingsController1', SettingsController1).factory('UserService', UserService);
-
-function SettingsController1($scope, UserService) {
-  $scope.users = UserService.all();
-    console.log($scope.users);
-};
-
-
-function UserService() {
-  var users = ["Peter", "Daniel", "Nina"];
-
-  return {
-    all: function() {
-      return users;
-    },
-    first: function() {
-      return users[0];
+var app = angular.module('controllerAsExample', ['ngResource']);
+app.factory('Person', ['$resource', function ($resource) {
+    return {
+        getAll: function(){
+            return $resource("http://localhost:3000/project")
+        }
     }
-  };
-};
+}]);
 
-
-
-//funktioniert
-/*angular.module('controllerAsExample', [])
-  .controller('SettingsController1', SettingsController1);
-
-function SettingsController1($scope, $http) {
-    console.log("controller aufgerufen")
-    $http.get('http://localhost:3000/project').
-        then(function(response) {
-            console.log(response.data.title);
-            $scope.greeting = response.data;
-        });
-}*/
-/*appIndex.controller('indexController', ['$scope', 'DataService', function ($scope, DataService) {
-    $scope.listProject = "";
-    //load all available Projects from server.
-    loadProjects();
-    function loadProjects()
-    {
-        DataService.getProject().then(function successCallback(response) {
-            if (response.data.length > 0) {
-                $scope.listProject = response.data;
-            } 
-        }, function errorCallback(response) {
-            //alert(response.status);
-        });
-    }
-}]);*/
+app.controller('SettingsController1', ['$scope', 'Person', function($scope, Person) {
+ Person.getAll().get().$promise.then(function(result) {
+    //success
+    $scope.bloglist = result.description;
+    console.log($scope.bloglist);
+  }, function(errResponse) {
+    // fail
+  });
+}]);   
