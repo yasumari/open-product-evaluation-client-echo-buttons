f0c8cc6181b4da8d94277dfcae7437671c1da4f1
cklein2 <corinna_sofie.klein@smail.fh-koeln.de>
2018-05-20 14:36:32 +0200

    in factory ausgelagert

--- ProjektEntwicklungAB/Client/Controllers/indexCtrl.js
+++ ProjektEntwicklungAB/Client/Controllers/indexCtrl.js
@@ -1,13 +1,39 @@
 angular.module('controllerAsExample', [])
+  .controller('SettingsController1', SettingsController1).factory('UserService', UserService);
+
+function SettingsController1($scope, UserService) {
+  $scope.users = UserService.all();
+    console.log($scope.users);
+};
+
+
+function UserService() {
+  var users = ["Peter", "Daniel", "Nina"];
+
+  return {
+    all: function() {
+      return users;
+    },
+    first: function() {
+      return users[0];
+    }
+  };
+};
+
+
+
+//funktioniert
+/*angular.module('controllerAsExample', [])
   .controller('SettingsController1', SettingsController1);
 
 function SettingsController1($scope, $http) {
+    console.log("controller aufgerufen")
     $http.get('http://localhost:3000/project').
         then(function(response) {
             console.log(response.data.title);
             $scope.greeting = response.data;
         });
-}
+}*/
 /*appIndex.controller('indexController', ['$scope', 'DataService', function ($scope, DataService) {
     $scope.listProject = "";
     //load all available Projects from server.
