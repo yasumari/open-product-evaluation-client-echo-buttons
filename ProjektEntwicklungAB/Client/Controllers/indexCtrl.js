c5283c35e61462e077699d2d21ef3e2482902157
cklein2 <corinna_sofie.klein@smail.fh-koeln.de>
2018-05-20 17:37:23 +0200

    in HTML test

--- ProjektEntwicklungAB/Client/Controllers/indexCtrl.js
+++ ProjektEntwicklungAB/Client/Controllers/indexCtrl.js
@@ -6,27 +6,10 @@ ContactController.$inject = ['$scope', 'Person'];
 function ContactController($scope, Person) {
  Person.getAll().get().$promise.then(function(result) {
     //success
-    $scope.bloglist = result.description;
-    console.log($scope.bloglist);
+     $scope.name=result.title;
+    $scope.description = result.description;
+    console.log($scope.name);
   }, function(errResponse) {
     // fail
   });
 }
-/*var app = angular.module('controllerAsExample', ['ngResource']);
-app.factory('Person', ['$resource', function ($resource) {
-    return {
-        getAll: function(){
-            return $resource("http://localhost:3000/project")
-        }
-    }
-}]);
-
-app.controller('SettingsController1', ['$scope', 'Person', function($scope, Person) {
- Person.getAll().get().$promise.then(function(result) {
-    //success
-    $scope.bloglist = result.description;
-    console.log($scope.bloglist);
-  }, function(errResponse) {
-    // fail
-  });
-}]); */
