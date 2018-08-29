2e469ee894182105d9d39b141ae226ffb5fd3e35
Cklein2 <corinna_sofie.klein@smail.th-koeln.de>
2018-08-29 16:17:43 +0200

    disable Buttons, added echoBtn 3+4 in angular

--- Client/src/app/Components/question/question.component.ts
+++ Client/src/app/Components/question/question.component.ts
@@ -21,14 +21,27 @@ export class QuestionComponent implements OnInit, OnDestroy {
   private currentAnswer: Answer;
   private currentQuestion: Question;
   private ranking=[]; 
-  private max_items;
+  private count_items;
 
  constructor(private apollo: Apollo, private renderer: Renderer2, private dataService: DataService, private router: Router, private messageService: MessageService) {
  }
+
+  //Progressbar
+  calculate():string {
+    return (this.dataService.getAnswerNumber()*100/this.currentProject.activeSurvey.questions.length)+"%";
+  }
+
   //btn_number sagt welches Item im Array gewählt wurde 
   //Button 0,1,2,3
   //       | | | |
   //Items  0,1,2,3
+
+  disableAllButtens(){
+    for (let btn of this.currentQuestion.items) {
+          let _btn: HTMLElement=document.getElementById(btn.image.id);
+          this.renderer.setProperty(_btn, 'disabled', 'true');
+        }
+  }
 
 favoriteQuestionClick(btn_number: number){
   this.apollo.mutate({
@@ -152,13 +165,13 @@ rankingQuestionClick(btn_number: number){
         this.router.navigate(['/feedback']);
       }, Constants.TIMER_QUESTION);
   } else {
-    this.max_items++;    
+    this.count_items++;    
   }
 }
 
    buttonClick(btn_number: number){    
     console.log("Button: " + btn_number);
-    //TODO Welches Bild wird sonst angezeigt?
+
     this.currentAnswer={
       questionID: this.currentQuestion.id,
       deviceID: this.dataService.getDeviceID(), 
@@ -169,7 +182,7 @@ rankingQuestionClick(btn_number: number){
       case 'RankingQuestion':
         const btn_rank: HTMLElement = document.getElementById(this.currentQuestion.items[btn_number].image.id);
         this.renderer.setStyle(btn_rank, 'background', 'lightgrey');
-        this.renderer.setProperty(btn_rank, 'innerHTML', 'Platz '+(this.max_items+1));
+        this.renderer.setProperty(btn_rank, 'innerHTML', 'Platz '+(this.count_items+1));
         this.renderer.setProperty(btn_rank, 'color', '#34a7bd');
         this.renderer.setProperty(btn_rank, 'disabled', 'true');
         this.rankingQuestionClick(btn_number);
@@ -187,13 +200,8 @@ rankingQuestionClick(btn_number: number){
       case 'RegulatorQuestion':
         this.sub.unsubscribe();
           //TODO Schöner gestalten Einfärben der Buttons bei Auswählen der Schritte
-        let i=0,j=0;
-        while(i<(btn_number+1)){
-          let btn_ranking: HTMLElement = document.getElementById("ranking_"+i);
-          this.renderer.setStyle(btn_ranking, 'background', '#34a7bd');
-          i++;
-        }
-        //Alle Disablen
+        let j=0;
+        //Alle 4 Buttons disablen
         while(j<4){
           let btn_reg: HTMLElement=document.getElementById("ranking_"+j);
           this.renderer.setProperty(btn_reg, 'disabled', 'true');
@@ -204,54 +212,32 @@ rankingQuestionClick(btn_number: number){
 
       case 'ChoiceQuestion': 
         this.sub.unsubscribe();
-        //TODO while raus in allen
-        i=0;
-        while(i<this.currentQuestion.items.length){
-          let btn_like1: HTMLElement=document.getElementById(this.currentQuestion.items[i].image.id);
-          this.renderer.setProperty(btn_like1, 'disabled', 'true');
-          i++;
-        }
+        this.disableAllButtens();
         this.choiceQuestionClick(btn_number);
         break;
 
       case 'LikeQuestion':
         this.sub.unsubscribe();
-        i=0;
-        while(i<this.currentQuestion.items.length){
-          let btn_like1: HTMLElement=document.getElementById(this.currentQuestion.items[i].image.id);
-          this.renderer.setProperty(btn_like1, 'disabled', 'true');
-          i++;
-        }
+        this.disableAllButtens();
         this.likeQuestionClick(btn_number);
         break;
 
       case 'FavoriteQuestion':
         this.sub.unsubscribe();
-        i=0;
-        while(i<this.currentQuestion.items.length){
-          let btn_fav: HTMLElement=document.getElementById(this.currentQuestion.items[i].image.id);
-          this.renderer.setProperty(btn_fav, 'disabled', 'true');
-          i++;
-        }
+        this.disableAllButtens();
         this.favoriteQuestionClick(btn_number);
         break;
       }
     }
 
 
-  //Progressbar
-  calculate():string {
-    return (this.dataService.getAnswerNumber()*100/this.currentProject.activeSurvey.questions.length)+"%";
-  }
-
  public ngOnInit(): void {
- 
       this.currentProject = this.dataService.getContext();
       this.currentQuestion = this.currentProject.activeSurvey.questions[this.dataService.getAnswerNumber()];
       if (this.currentQuestion.__typename=="RankingQuestion"){
-        this.max_items= 0;
+        this.count_items= 0;
       }
-            //TODO rausnehmen, nur für testdaten drin
+      //TODO rausnehmen, nur für testdaten drin
       this.currentQuestion.items[0].image.url="../../../assets/images/checklist-1295319_1280.png";
       this.currentQuestion.items[1].image.url="../../../assets/images/checklist-2023731_1280.png";
 
