0c927b783bbee9044ff0def807e061cdd1996a19
Cklein2 <corinna_sofie.klein@smail.th-koeln.de>
2018-09-25 08:54:19 +0200

    changed Timer

--- Client/src/app/constants.ts
+++ Client/src/app/constants.ts
@@ -1,7 +1,7 @@
 export class Constants {
     public static get SERVER_URL(): string { return "http://localhost:3000/"; };
     public static get SOCKETS_URL(): string { return "http://localhost:3001"; };
-    public static get TIMER_QUESTION(): number { return 40000; };
+    public static get TIMER_QUESTION(): number { return 4000; };
     public static get TIMER_FEEDBACK(): number { return 40000; };
     public static get TIMER_END(): number { return 60000; };
   }
